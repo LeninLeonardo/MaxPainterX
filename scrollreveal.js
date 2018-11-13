afb435654e31039ca995875e778371ba3f14cd31
LeninLeonardo <35757388+LeninLeonardo@users.noreply.github.com>
2018-11-13 00:48:29 -0400

    Add files via upload

---
+++ scrollreveal.js
@@ -0,0 +1,27 @@
+window.sr = ScrollReveal();
+
+sr.reveal(".logo", {
+    duration:2000,
+    origin:"left",
+    distance: '300px'
+});
+
+sr.reveal(".title-banner, .text_banner, .text-banner, .botones, .main-menu", {
+    duration:2000,
+    origin:"left",
+    distance: '300px'
+});
+
+sr.reveal(".left", {
+    duration:2000,
+    origin:"left",
+    distance: '300px'
+});
+
+sr.reveal(".right", {
+    duration:2000,
+    origin:"right",
+    distance: '300px'
+});
+
+
